193a7809749f784cbe91c5a1ebae717ba6db5f09
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-06 20:40:27 -0500

    first commit

---
+++ StringBuilderCs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringBuilderCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
